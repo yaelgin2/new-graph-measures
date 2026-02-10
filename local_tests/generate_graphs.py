721b95360862241fad47b8c74c2951a92b5bd02d
Tal Cohen(cohent59) <cohent59@peptibase10.math.biu.ac.il>
2026-02-10 14:20:10 +0200

    Updated test scripts

--- local_tests/generate_graphs.py
+++ local_tests/generate_graphs.py
@@ -7,7 +7,7 @@ import json
 from collections import deque, defaultdict
 import inspect
 import os, sys
-OUTPUT_DIR = r"/home/cohent59/new-graph-measures/local_tests/input_color_uniform_deg_15"
+#OUTPUT_DIR = r"/home/cohent59/new-graph-measures/local_tests/subgraphs_avg_deg_5_uniform"
 
 # Create Colored Graph G(n,p)
 def create_colored_graph(n,p):
@@ -25,8 +25,10 @@ def create_colored_graph(n,p):
             0.05, 0.05, 0.05, 0.05, 0.05,  # 5 dominant colors (75%)
             0.05, 0.05, 0.05, 0.05, 0.05,  # medium colors
             0.05, 0.05, 0.05, 0.05, 0.05,  # rare
-            0.05,0.05,0.05,0.05,0.05  # very rare
+            0.05, 0.05, 0.05, 0.05, 0.05  # very rare
             ]
+
+
 
 
     G = nx.erdos_renyi_graph(n=n, p=p)
@@ -157,28 +159,30 @@ def read_graph_file(filename):
 
 # Run
 if __name__ == "__main__":
-    n = 50000
-    avg_neighbors = 8
-    G = create_colored_graph(n, float(avg_neighbors) / n)
-    print("done generating G")
+    for k in [5]:
+        for j in range(1, 10):
+            n = 50000
+            avg_neighbors = k
+            G = create_colored_graph(n, float(avg_neighbors) / n)
+            print("done generating G")
 
-    sizeL=1500
-    sizeH=2000
-    count_S=1000
-     #embed sub-graphs
-    how_many_to_embed = 10
+            sizeL=1500
+            sizeH=2000
+            count_S=1000
+            #embed sub-graphs
+            how_many_to_embed = 10
 
-    # s_list=generate_and_save_subgraphs(count_S, avg_neighbors,sizeL,sizeH,OUTPUT_DIR,how_many_to_embed)
-    # print("done generating s_list")
+            # s_list=generate_and_save_subgraphs(count_S, avg_neighbors,sizeL,sizeH,OUTPUT_DIR,how_many_to_embed)
+            # print("done generating s_list")
 
-    s_list = [read_graph_file(f"/home/cohent59/new-graph-measures/local_tests/input_color_uniform_deg_3/S_{i}.json") for i in range(1, 11)]
+            s_list = [read_graph_file(f"/home/cohent59/new-graph-measures/local_tests/input_color_uniform_deg_3/S_{i}.json") for i in range(1, 11)]
 
-    available_nodes = set(G.nodes())
-    for i in range(how_many_to_embed):
-        print(f"embed s number {i+1}")
-        available = embed_subgraph(G, s_list[i],available_nodes)
-        available_nodes = available
+            available_nodes = set(G.nodes())
+            for i in range(how_many_to_embed):
+                print(f"embed s number {i+1}")
+                available = embed_subgraph(G, s_list[i],available_nodes)
+                available_nodes = available
 
-    data_G = graph_to_json_struct(G)
-    save_json(data_G, f"/home/cohent59/new-graph-measures/local_tests/graphs_by_density/g_den_8_embedded_den_3_uniform.json")
-    print("DONE: G and all S_i saved.")
+            data_G = graph_to_json_struct(G)
+            save_json(data_G, f"/home/cohent59/new-graph-measures/local_tests/graphs_by_density/g_den_{k}_embedded_den_3_uniform_{j}.json")
+            print("DONE: G and all S_i saved.")
